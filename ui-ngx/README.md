82f3187ca1fdeae52a842c8efde439dbbb676294
michael <michael@designquest.com.hk>
2024-08-10 09:48:06 +0800

    change color theme

---
+++ ui-ngx/README.md
@@ -0,0 +1,5 @@
+# Notes:
+
+## Faile to connect oauth2Clients
+ - Start the thingsboard server on 8080 port for login
+ - update the server address in the proxy.conf.js file
--- ui-ngx/proxy.conf.js
+++ ui-ngx/proxy.conf.js
@@ -13,35 +13,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const forwardUrl = "http://localhost:8080";
-const wsForwardUrl = "ws://localhost:8080";
+const forwardUrl = "http://192.168.196.95:8080";
+const wsForwardUrl = "ws://192.168.196.95:8080";
 const ruleNodeUiforwardUrl = forwardUrl;
 
 const PROXY_CONFIG = {
   "/api": {
-    "target": forwardUrl,
-    "secure": false,
+    target: forwardUrl,
+    secure: false,
   },
   "/static/rulenode": {
-    "target": ruleNodeUiforwardUrl,
-    "secure": false,
+    target: ruleNodeUiforwardUrl,
+    secure: false,
   },
   "/static/widgets": {
-    "target": forwardUrl,
-    "secure": false,
+    target: forwardUrl,
+    secure: false,
   },
   "/oauth2": {
-    "target": forwardUrl,
-    "secure": false,
+    target: forwardUrl,
+    secure: false,
   },
   "/login/oauth2": {
-    "target": forwardUrl,
-    "secure": false,
+    target: forwardUrl,
+    secure: false,
   },
   "/api/ws": {
-    "target": wsForwardUrl,
-    "ws": true,
-    "secure": false
+    target: wsForwardUrl,
+    ws: true,
+    secure: false,
   },
 };
 
